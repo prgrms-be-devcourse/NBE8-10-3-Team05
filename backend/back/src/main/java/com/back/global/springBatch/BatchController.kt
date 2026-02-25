cdc4323479bbfdb7e96fc68f4b4092d49eaf97ab
gurum505 <sayhojeong505@gmail.com>
2026-02-25 14:31:37 +0900

    refactor: spring batch controller 변경

--- backend/back/src/main/java/com/back/global/springBatch/BatchController.kt
+++ backend/back/src/main/java/com/back/global/springBatch/BatchController.kt
@@ -1,27 +1,26 @@
-package com.back.global.springBatch;
+package com.back.global.springBatch
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-@RequiredArgsConstructor
-@Slf4j
-public class BatchController {
-    private final BatchJobLauncher batchJobLauncher;
-
+class BatchController(private val batchJobLauncher: BatchJobLauncher) {
     @GetMapping("/batchTest")
     @ResponseBody
-    public String setup() {
-        log.info(">>> 사용자 요청: 배치 프로세스 가동");
+    fun setup(): String {
+        log.info(">>> 사용자 요청: 배치 프로세스 가동")
 
         // 비동기로 실행 (배치 실행 메서드에 @Async가 붙어있어야 함)
-        batchJobLauncher.runJob();
+        batchJobLauncher.runJob()
 
         // 브라우저에는 즉시 응답 반환
-        return "null";
+        return "null"
+    }
+
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(BatchController::class.java)
     }
 }
