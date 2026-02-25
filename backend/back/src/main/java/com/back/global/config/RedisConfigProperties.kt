b2bd85f11ada4ecdb7422714ce12aaead0d14128
gurum505 <sayhojeong505@gmail.com>
2026-02-25 15:29:12 +0900

    refactor: redis config변경

--- backend/back/src/main/java/com/back/global/config/RedisConfigProperties.kt
+++ backend/back/src/main/java/com/back/global/config/RedisConfigProperties.kt
@@ -1,6 +1,6 @@
-package com.back.global.config;
+package com.back.global.config
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("spring.data.redis")
-public record RedisConfigProperties(String host, int port) {}
+data class RedisConfigProperties(val host: String, val port: Int)
