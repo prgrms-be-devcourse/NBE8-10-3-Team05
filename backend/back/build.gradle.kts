c9eac549f4f5d3cd7b780f5f9b7f255bb02c490e
lonelylonelylonely <lonelylonelylonely@example.com>
2026-02-27 16:33:34 +0900

    refactor : SecurityConfig 수정 & build.gradle 의존성 추가

--- backend/back/build.gradle.kts
+++ backend/back/build.gradle.kts
@@ -156,6 +156,10 @@ dependencies {
     implementation("org.elasticsearch.client:elasticsearch-rest-client:8.11.3")
     implementation("org.apache.httpcomponents.client5:httpclient5")
 
+    // prometheus
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+
 }
 
 tasks.withType<Test> {
