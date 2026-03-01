30d120c5b5e47a5747e1352ad2af7d271a0b67f5
gurum505 <sayhojeong505@gmail.com>
2026-03-01 15:57:49 +0900

    fix : estate json parse 오류생기던 문제 수정

--- backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateFetchResponseDto.kt
+++ backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateFetchResponseDto.kt
@@ -3,22 +3,31 @@ package com.back.domain.welfare.estate.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class EstateFetchResponseDto(
+    @field:JsonProperty("response")
     val response: Response? = null
 ) {
     data class Response(
+        @field:JsonProperty("header")
         val header: HeaderDto,
+        @field:JsonProperty("body")
         val body: BodyDto
     ) {
         data class HeaderDto(
+            @field:JsonProperty("resultCode")
             val resultCode: String,
+            @field:JsonProperty("resultMsg")
             val resultMsg: String
         )
 
         data class BodyDto(
+            @field:JsonProperty("numOfRows")
             val numOfRows: String,
+            @field:JsonProperty("pageNo")
             val pageNo: String,
+            @field:JsonProperty("totalCount")
             val totalCount: String,
-            val items: List<EstateDto>// MutableList일 필요가 없다면 List 권장
+            @field:JsonProperty("item")
+            val items: List<EstateDto>
         )
     }
 }
