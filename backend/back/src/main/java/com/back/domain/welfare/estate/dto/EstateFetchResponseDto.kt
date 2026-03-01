16187ec7f143400d7d19a82fde169622a1ae05e0
gurum505 <sayhojeong505@gmail.com>
2026-03-01 14:36:56 +0900

    fix : estate dto jsonproperty문제 수정

--- backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateFetchResponseDto.kt
+++ backend/back/src/main/java/com/back/domain/welfare/estate/dto/EstateFetchResponseDto.kt
@@ -3,22 +3,22 @@ package com.back.domain.welfare.estate.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class EstateFetchResponseDto(
-    @JvmField val response: Response? = null
+    val response: Response? = null
 ) {
     data class Response(
-        val header: HeaderDto? = null,
-        @JvmField val body: BodyDto? = null
+        val header: HeaderDto,
+        val body: BodyDto
     ) {
         data class HeaderDto(
-            val resultCode: String? = null,
-            val resultMsg: String? = null
+            val resultCode: String,
+            val resultMsg: String
         )
 
         data class BodyDto(
-            @JvmField val numOfRows: String? = null,
-            @JvmField val pageNo: String? = null,
-            @JvmField val totalCount: String? = null,
-            @JvmField val items: List<EstateDto?>? = null // MutableList일 필요가 없다면 List 권장
+            val numOfRows: String,
+            val pageNo: String,
+            val totalCount: String,
+            val items: List<EstateDto>// MutableList일 필요가 없다면 List 권장
         )
     }
 }
